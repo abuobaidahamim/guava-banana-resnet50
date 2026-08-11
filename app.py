b0f67d9bfe651392b26aba0d67722375e36f5e38
abuobaidahamim <abuobaidahamim@gmail.com>
2026-08-11 06:09:40 +0600

    Load model without compilation

--- app.py
+++ app.py
@@ -39,7 +39,10 @@ def load_model():
         model_url
     )
 
-    model = tf.keras.models.load_model(model_path)
+    model = tf.keras.models.load_model(
+        model_path,
+        compile=False
+    )
 
     return model
 
